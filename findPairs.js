c61053538075d2b03633b11989142ea31ba1524a
shihabus <shihabuudheen@gmail.com>
2019-02-28 01:26:35 +0530

    final push

--- findPairs.js
+++ findPairs.js
@@ -1,14 +1,19 @@
+// ================================================
+// This functions is used to find the pairs,
+// which a difference equal to a traget value.
+// ================================================
+
+// It takes two arguments, thee array and the target value
 findPairs=(arr, target)=>{
-    let result = []; 
-    let unique = [...new Set(arr)]
-    let nums = unique.sort()
-    for (var i = 0; i < nums.length; i++) {
-        for (var j = nums.length- 1; j > i; j--) {
-            if (nums[j] - nums[i] === target) {
-                result.push([nums[j],nums[i]]);
+    let result = []; //to store the final result
+    let unique = [...new Set(arr)]  //removing repeated array elements
+    let array = unique.sort()  //ascend sort of array
+    for (let i = 0; i < array.length; i++) {
+        for (let j = array.length- 1; j > i; j--) {
+            if (array[j] - array[i] === target) {
+                result.push([array[j],array[i]]);  //pushing the pair which meets the condition
             }
         }
     }
     return result;
 }
-console.log(findPairs([4,3,2,1,2,2,1,4], 1));
